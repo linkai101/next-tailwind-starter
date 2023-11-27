b08fee5e7eeaed65003f720daa916c236cc8391e
Linkai Wu <linkai@linkaiwu.com>
2023-11-27 13:33:52 -0600

    Update versions

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,26 @@
 import Head from 'next/head';
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'next-tailwind-starter',
+  description: 'A Next.js + TypeScript + Tailwind starter by @linkai101',
+};
 
 export default function Home() {
   return <>
     <Head>
       <title>next-tailwind-starter</title>
-      <meta name="description" content="by @linkai101 on github" />
+      <meta name="description" content="by @linkai101" />
     </Head>
 
     <main>
       <div className="container px-8 py-8">
-        <h1 className="text-3xl">next-tailwind-starter <span className="text-lg text-gray-400">v1</span></h1>
-        <p className="text-md">
-          by <a href="https://github.com/linkai101" className="font-bold text-blue-500 hover:underline" target="_blank">@linkai101</a>
-        </p>
+        <h1 className="text-3xl font-bold">
+        <a href="https://github.com/linkai101" className="text-blue-500 hover:underline" target="_blank">linkai101</a>/next-tailwind-starter
+        </h1>
 
-        <p className="mt-4">
-          A starter Next.js project installed with TypeScript and Tailwind.
+        <p className="">
+          installed with Next.js (Using Pages Router), TypeScript, Tailwind CSS
         </p>
       </div>
     </main>
